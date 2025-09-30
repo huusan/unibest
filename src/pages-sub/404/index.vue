2b8acd76fa6aba7691ac84768dc067a9db0a5d88
feng <@example.com>
2025-09-30 13:44:54 +0800

    refactor(404): 使用wot-design组件替换页面元素

--- src/pages-sub/404/index.vue
+++ src/pages-sub/404/index.vue
@@ -17,11 +17,10 @@ function goBack() {
 
 <template>
   <view class="h-screen flex flex-col items-center justify-center">
-    <view> 404 </view>
-    <view> 页面不存在 </view>
-    <button class="mt-6 w-40 text-center" @click="goBack">
+    <wd-status-tip image="content" tip="页面不存在!" />
+    <wd-button class="mt-6 w-40 text-center" @click="goBack">
       返回首页
-    </button>
+    </wd-button>
   </view>
 </template>
 
